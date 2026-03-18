c98c8624c0b59b1b38f41d5bdb583a5657b31392
Madhu1524 <madhumathy247@gmail.com>
2026-03-18 17:37:38 +0530

    basic printf/scanf

---
+++ 18Mar-C/c_main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* 1. printf statement using printf*/
+/*
+int main()
+{
+    printf( "hi ninja !!\n");
+    printf( "welcome to c programming");
+    return 0;
+}
+*/
+
+/* 2. Take input using scanf*/
+
+int main() 
+{
+    int num;
+    printf("Enter a No:");
+    scanf("%d", &num);
+    printf("Your No is %d", num);
+    return 0;
+}
