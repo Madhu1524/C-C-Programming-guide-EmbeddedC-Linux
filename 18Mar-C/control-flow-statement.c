e07eaaabd9a8b04647dd2e16a6393480127694bf
Madhu1524 <madhumathy247@gmail.com>
2026-03-20 11:20:22 +0530

    control-statement

---
+++ 18Mar-C/control-flow-statement.c
@@ -0,0 +1,16 @@
+/* Control flow statements in Programming 
+   Types of Control Flow statements in C - If-Else, Switch, L*/
+
+   #include <stdio.h>
+
+   int main()
+   {
+    int a = 10;
+    if (a == 10) {
+    printf("a is equal to 10");
+    }
+    else if (a != 10) {
+        printf("a is not equal to 10");
+    }
+       return 0;
+   }
